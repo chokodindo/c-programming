0dc031ac2e861c32ee3c9cab545c36c07f28dd95
chokodindo <122534080+chokodindo@users.noreply.github.com>
2023-01-31 23:59:40 +0300

    Add files via upload

---
+++ simple intrest.c
@@ -0,0 +1,17 @@
+// program to calculate simple intrest
+#include <stdio.h>
+int main()
+{
+  float t,r,p,intrest;
+  printf("\nenter the principal amount: ");
+  scanf("%f",&p);
+  printf("\nenter the time value: ");
+  scanf("%f",&t);
+  printf("\nenter the rate value: ");
+  scanf("%f",&r);
+  interst=p*t*r;
+  printf("intrest=*f",intrest);
+  return 0;
+  
+}
+  
